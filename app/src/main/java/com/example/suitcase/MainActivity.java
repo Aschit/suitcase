ed4a310ee66194a726d90b00af4fb0670f3c24d9
Suicase <aschitdhakal@gmail.com>
2023-08-30 08:45:53 +0545

    Main java

--- app/src/main/java/com/example/suitcase/MainActivity.java
+++ app/src/main/java/com/example/suitcase/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.suitcase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.suitcase.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+
 
 
 
@@ -16,6 +27,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int id= item.getItemId();
+                if(id==R.id.item_home){
+                    Intent intent= new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(MainActivity.this, "Click to Home", Toast.LENGTH_SHORT).show();
+                }
+                if (id==R.id.item_about){
+                    Toast.makeText(MainActivity.this, "Click to About", Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+        });
+        final DrawerLayout drawerLayout=findViewById(R.id.drawer);
+        findViewById(R.id.navmenu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(getApplicationContext(), add_items.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
